6e66400902be31beec980ee720f2e40713d4ccb4
unknown <peterataras@gmail.com>
2023-03-15 16:55:35 -0700

    main handlebar functional

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const path = require('path');
+
+
+const helpers = require('./utils/helpers');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({helpers});
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3101;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 36000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -19,12 +26,16 @@ const sess = {
 };
 
 app.use(session(sess));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () =>   console.log(`Server running on port ${PORT}`))
 });
